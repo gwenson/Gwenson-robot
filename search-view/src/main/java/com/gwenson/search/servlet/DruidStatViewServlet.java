6e0ceb7d0addc20e4c4144a9a545ad464bcbbcf0
Gwenson <gwenson@163.com>
2017-12-06 14:10:58 +0800

    Update DruidStatViewServlet.java

--- search-view/src/main/java/com/gwenson/search/servlet/DruidStatViewServlet.java
+++ search-view/src/main/java/com/gwenson/search/servlet/DruidStatViewServlet.java
@@ -10,13 +10,13 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(urlPatterns="/druid/*",  
         initParams={  
                 /** 白名单，如果不配置或value为空，则允许所有 */  
-                @WebInitParam(name="allow",value="127.0.0.1,192.0.0.1,119.23.19.172"),  
+                @WebInitParam(name="allow",value="127.0.0.1,192.0.0.1"),  
                 /** 黑名单，与白名单存在相同IP时，优先于白名单 */  
                 @WebInitParam(name="deny",value="192.0.0.1"),  
                 /** 用户名 */  
                 @WebInitParam(name="loginUsername",value="root"),  
                 /** 密码 */  
-                @WebInitParam(name="loginPassword",value="gwenson@5832832"),  
+                @WebInitParam(name="loginPassword",value="123456"),  
                 /** 禁用HTML页面上的“Reset All”功能   */  
                 @WebInitParam(name="resetEnable",value="false")  
         })  
